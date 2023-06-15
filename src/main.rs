f896971051e2ed40e628f661d16e7cce83536d35
Joseph Louthan <joe@theologic.us>
2023-06-15 06:17:57 -0400

    initial - first learning structs

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,23 @@ struct User {
     sign_in_count: u64,
 }
 
-fn main() {}
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
+
+fn main() {
+    let user1 = build_user(
+        String::from("joe@theologic.us"),
+        String::from("joseph"),
+    );
+
+    let user2 = User {
+        email: String::from("joelouthan@proton.me"),
+        ..user1
+    };
+}
